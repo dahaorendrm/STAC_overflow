49db42bfa94663ec2a60c9afb9d281d176a7d039
xm home <dahaorendrm@gmail.com>
2021-08-30 21:50:07 -0400

    process csv file

---
+++ utils.py
@@ -0,0 +1,122 @@
+import rasterio
+
+
+# Helper functions for visualizing Sentinel-1 images
+def scale_img(matrix):
+    """
+    Returns a scaled (H, W, D) image that is visually inspectable.
+    Image is linearly scaled between min_ and max_value, by channel.
+
+    Args:
+        matrix (np.array): (H, W, D) image to be scaled
+
+    Returns:
+        np.array: Image (H, W, 3) ready for visualization
+    """
+    # Set min/max values
+    min_values = np.array([-23, -28, 0.2])
+    max_values = np.array([0, -5, 1])
+
+    # Reshape matrix
+    w, h, d = matrix.shape
+    matrix = np.reshape(matrix, [w * h, d]).astype(np.float64)
+
+    # Scale by min/max
+    matrix = (matrix - min_values[None, :]) / (
+        max_values[None, :] - min_values[None, :]
+    )
+    matrix = np.reshape(matrix, [w, h, d])
+
+    # Limit values to 0/1 interval
+    return matrix.clip(0, 1)
+
+
+def create_false_color_composite(path_vv, path_vh):
+    """
+    Returns a S1 false color composite for visualization.
+
+    Args:
+        path_vv (str): path to the VV band
+        path_vh (str): path to the VH band
+
+    Returns:
+        np.array: image (H, W, 3) ready for visualization
+    """
+    # Read VV/VH bands
+    with rasterio.open(path_vv) as vv:
+        vv_img = vv.read(1)
+    with rasterio.open(path_vh) as vh:
+        vh_img = vh.read(1)
+
+    # Stack arrays along the last dimension
+    s1_img = np.stack((vv_img, vh_img), axis=-1)
+
+    # Create false color composite
+    img = np.zeros((512, 512, 3), dtype=np.float32)
+    img[:, :, :2] = s1_img.copy()
+    img[:, :, 2] = s1_img[:, :, 0] / s1_img[:, :, 1]
+
+    return scale_img(img)
+
+
+def display_random_chip(random_state):
+    """
+    Plots a 3-channel representation of VV/VH polarizations as a single chip (image 1).
+    Overlays a chip's corresponding water label (image 2).
+
+    Args:
+        random_state (int): random seed used to select a chip
+
+    Returns:
+        plot.show(): chip and labels plotted with pyplot
+    """
+    f, ax = plt.subplots(1, 2, figsize=(9, 9))
+
+    # Select a random chip from train_metadata
+    random_chip = train_metadata.chip_id.sample(random_state=random_state).values[0]
+    chip_df = train_metadata[train_metadata.chip_id == random_chip]
+
+    # Extract paths to image files
+    vv_path = chip_df[chip_df.polarization == "vv"].feature_path.values[0]
+    vh_path = chip_df[chip_df.polarization == "vh"].feature_path.values[0]
+    label_path = chip_df.label_path.values[0]
+
+    # Create false color composite
+    s1_img = create_false_color_composite(vv_path, vh_path)
+
+    # Visualize features
+    ax[0].imshow(s1_img)
+    ax[0].set_title("S1 Chip", fontsize=14)
+
+    # Load water mask
+    with rasterio.open(label_path) as lp:
+        lp_img = lp.read(1)
+
+    # Mask missing data and 0s for visualization
+    label = np.ma.masked_where((lp_img == 0) | (lp_img == 255), lp_img)
+
+    # Visualize water label
+    ax[1].imshow(s1_img)
+    ax[1].imshow(label, cmap="cool", alpha=1)
+    ax[1].set_title("S1 Chip with Water Label", fontsize=14)
+
+    plt.tight_layout(pad=5)
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    # Examine an arbitrary image
+    image_path = train_metadata.feature_path[0]
+    with rasterio.open(image_path) as img:
+        metadata = img.meta
+        bounds = img.bounds
+        data = img.read(1)  # read a single band
+    print(metadata)
+
+    with rasterio.open(image_path) as img:
+        gdal_mask = img.dataset_mask()
+
+    with rasterio.open(image_path) as img:
+        numpy_mask = img.read(1, masked=True)
+    display_random_chip(7)
