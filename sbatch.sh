7dd020d790713d93824471e5387d5d314fca6582
XM darwin <xmdrm@udel.edu>
2021-09-09 22:34:59 -0400

    add move files

---
+++ sbatch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#BATCH --nodes=1
+#SBATCH --ntasks=1
+
+#SBATCH --cpus-per-task=8
+
+#SBATCH --mem=40G
+# SBATCH --mem-per-cpu=10G
+
+#SBATCH --job-name=STAC_train
+#SBATCH --partition=gpu-t4
+#SBATCH --gpus=1
+
+#SBATCH --time=0-2:00:00
+# SBATCH --output=ArraySCI%A-%a.out
+#SBATCH --mail-user='xmdrm@udel.edu'
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_90
+
+# export OMP_NUM_THREADS=4
+vpkg_require xm_pytorch/20210902-STAC
+python3 -u stac_train.py
