d53fc6d95636ef8a051100997670fc46b3312976
XM darwin <xmdrm@udel.edu>
2021-09-13 11:37:33 -0400

    fix some small bugs for submissionwq

--- submit.py
+++ submit.py
@@ -1,7 +1,7 @@
 
 from  pathlib import Path
 import shutil
-
+import os
 
 
 
@@ -13,4 +13,6 @@ submission_assets_path.mkdir(exist_ok=True)
 weight_path = submission_assets_path / "flood_model.pt"
 weight_saved_path = 'model-outputs/flood_model.pt'
 shutil.copyfile(weight_saved_path,weight_path)
+if os.path.exists(submission_assets_path / "torch"):
+    shutil.rmtree(submission_assets_path / "torch")
 shutil.copytree('/home/1796/.cache/torch',submission_assets_path / "torch")
