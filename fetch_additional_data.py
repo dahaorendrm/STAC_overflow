ec8fa93eb6def582917010385f0f55acf34edbb5
冬 王 <Dong@dong-MBP.fios-router.home>
2021-09-15 21:22:22 -0400

    make fetch actionable

--- fetch_additional_data.py
+++ fetch_additional_data.py
@@ -153,3 +153,53 @@ def create_chip_aux_file(
         write_vrt(items, asset_key, vrt_path)
         reproject_to_chip(chip_info, vrt_path, output_path, resampling=resampling)
     return output_path
+
+
+# Download the flood-train-images.tgz file from competition Data Download page and
+# upload it to the Hub in the same directory as this notebook.
+
+# Then run the following code to uncompress this. Afterwards you should see an
+# train_features directory containing all of the training chips ending in .tif.
+# !tar -xvf flood-train-images.tgz
+
+# Use this directory to define the location of the chips, or if you have
+# already uncompressed the chips elsewhere set the location here:
+TRAINING_DATA_DIR = "training_data/train_features"
+
+# Gather chip paths
+# These chip paths will be used later in the notebook to process the chips.
+# These paths should be to only one GeoTIFF per chip; for example, if both
+# VV.tif and VH.tif are available for a chip, use only one of these paths.
+# The GeoTIFFs at these paths will be read to get the bounds, CRS and resolution
+# that will be used to fetch auxiliary input data. These can be relative paths.
+# The auxiliary input data will be saved in the same directory as the GeoTIFF
+# files at these paths.
+chip_paths = []
+for file_name in os.listdir(TRAINING_DATA_DIR):
+    if file_name.endswith("_vv.tif"):
+        chip_paths.append(os.path.join(TRAINING_DATA_DIR, file_name))
+print(f"{len(chip_paths)} chips found.")
+
+# Configurate the auxiliary input files that we will generate.
+# Define a set of parameters to pass into create_chip_aux_file
+aux_file_params = [
+    ("nasadem", "elevation", "nasadem.tif", Resampling.bilinear),
+    ("jrc-gsw", "extent", "jrc-gsw-extent.tif", Resampling.nearest),
+    ("jrc-gsw", "occurrence", "jrc-gsw-occurrence.tif", Resampling.nearest),
+    ("jrc-gsw", "recurrence", "jrc-gsw-recurrence.tif", Resampling.nearest),
+    ("jrc-gsw", "seasonality", "jrc-gsw-seasonality.tif", Resampling.nearest),
+    ("jrc-gsw", "transitions", "jrc-gsw-transitions.tif", Resampling.nearest),
+    ("jrc-gsw", "change", "jrc-gsw-change.tif", Resampling.nearest),
+]
+
+# Generate auxiliary input chips for NASADEM and JRC
+# Iterate over the chips and generate all aux input files.
+count = len(chip_paths)
+for i, chip_path in enumerate(chip_paths):
+    print(f"({i+1} of {count}) {chip_path}")
+    chip_info = get_chip_info(chip_path)
+    for collection_id, asset_key, file_name, resampling_method in aux_file_params:
+        print(f"  ... Creating chip data for {collection_id} {asset_key}")
+        create_chip_aux_file(
+            chip_info, collection_id, asset_key, file_name, resampling=resampling_method
+        )
