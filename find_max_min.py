ee4631e09006428750e55fe1571951f7ccfab9f2
XM darwin <xmdrm@udel.edu>
2021-09-21 21:18:10 -0400

    fix bug

--- find_max_min.py
+++ find_max_min.py
@@ -10,7 +10,7 @@ def find_max_min(path, name):
     li_min = []
     for data in data_list:
         if name in data:
-            with rasterio.open(data) as f:
+            with rasterio.open(path+'/'+data) as f:
                 img = f.read(1)
             li_max.append(np.amax(img))
             li_min.append(np.amin(img))
@@ -20,5 +20,5 @@ def find_max_min(path, name):
 
 if __name__ == '__main__':
     data_name = 'nasadem'
-    li_max,li_min = find_max_min('/training_data/train_features', data_name)
+    li_max,li_min = find_max_min('training_data/train_features', data_name)
     print(f'{data_name} max:{np.amax(li_max)},  min:{np.amin(li_min)}')
